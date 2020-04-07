5c4739b27e4927913e64ec8fe75af4abc7bc4d77
smriti chawla <smriti.chawla@tothenew.com>
2020-04-07 17:32:21 +0530

    started testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+// it('renders without crashing', ()=>{
+//   const wrapper = shallow(<App />)
+//   // console.log(wrapper.debug())
+//   expect(wrapper).toBeTruthy()
+//
+// })
+
+test('renders without error', ()=>{
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find('[data-test="component-app"]') ;//kabab case
+  expect(appComponent.length).toBe(1)
 });
+
+test('renders increment button' , ()=>{
+
+});
+
+test('renders display counter', ()=>{})
+
+test('counter starts at 0', ()=>{})
+
+test('clicking button increments counter display, state', ()=>{})
