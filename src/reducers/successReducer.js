177572456ee30d270758cc0e26d5e47bf0fe67d4
smriti chawla <smriti.chawla@tothenew.com>
2020-04-16 16:50:18 +0530

    redux setup

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function successReducer
+ * @param {array} state - array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @return {boolean} - new success state.
+ */
+
+export default (state, action) => {
+    return null;
+}
